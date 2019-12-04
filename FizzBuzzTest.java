e096d2abe237bcca9cbec9ec348f6d15027a55af
Alejandro Ojea <alojea@gmail.com>
2019-12-04 20:19:12 +0000

    Update FizzBuzzTest.java

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class HikerTest {
+public class FizzBuzzTest {
 
     @Test
     void test_FizzBuzz() {
